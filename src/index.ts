f9c9048f99ae236c05d269ba197c69bc432c1d93
chandan ojha <chandanojha1996@gmail.com>
2026-01-08 11:54:30 +0530

    1. Add Readme 2. Modify package json 3. Add flow chart

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { CronParser } from './core/CronParser';
 import { OutputFormatter } from './core/OutputFormatter';
 
 const input = process.argv[2];
-
 if (!input) {
   console.error('plese use: node dist/index.js "<cron expression>"');
   process.exit(1);
@@ -14,3 +13,21 @@ const expanded = cron.expand();
 const output = OutputFormatter.format(expanded);
 
 console.log(output);
+
+// CLI Input
+//   |
+// CliRunner
+//   |
+// CronParser
+//   |
+// CronExpression
+//   |
+// CronField.expand()
+//   |
+// FieldExpander
+//   |
+// Expanded Values
+//   |
+// OutputFormatter
+//   |
+// Console Output
